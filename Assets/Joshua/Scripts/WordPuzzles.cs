c4c1e9c1a0a7a4f9a7a97f922bd1a6f13605af88
Joshua Heideman <jcheideman@ucdavis.edu>
2018-01-27 17:18:07 -0800

    commit changes in my branch

--- Assets/Joshua/Scripts/WordPuzzles.cs
+++ Assets/Joshua/Scripts/WordPuzzles.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WordPuzzles : MonoBehaviour {
+public class WordPuzzles : PuzzleManager {
 	protected Dictionary<int,string> words;
 	protected bool isCorrect;
 	protected Text ConfirmationText;
